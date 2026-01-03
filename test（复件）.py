c16f8df194b2f3f7bdfbf54f158834f6df56a43a
lgl531558674 <531558674@qq.com>
2026-01-03 00:05:43 -0800

    新增一个test文件

---
+++ test（复件）.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 18 19:20:37 2025
+
+@author: zlzk
+"""
+
+
+import csv
+import os
+import sys
+                
+def main():
+    print("hello github")
+    print("新增一行代码")
+                
+# 在代码的最后添加
+if __name__ == "__main__":
+    
+    main()
+    
+   
